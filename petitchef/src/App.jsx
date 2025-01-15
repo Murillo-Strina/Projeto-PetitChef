4e113378ee1ce8feadd62dd669490fa1c68cc4b0
danilospinola <danspinola10@gmail.com>
2025-01-15 13:11:24 -0300

    .

--- petitchef/src/App.jsx
+++ petitchef/src/App.jsx
@@ -1,15 +1,20 @@
-import { useState } from 'react'
-import './App.css'
-import './components/Main'
-import ReactDOM from 'react-dom';
-import Main from './components/Main'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Main from './components/Main';
+import Login from './components/Login'; // Import your Login component
 
 function App() {
   return (
-    <>
-      <Main/>
-    </>
-  )
+    <Router>
+      <Routes>
+        {/* Define the route for the login page */}
+        <Route path="/login" element={<Login />} />
+
+        {/* Define the route for the main page */}
+        <Route path="/" element={<Main />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
