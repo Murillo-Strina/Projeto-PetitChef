2288396795576f785e20ff3b65bceb57a83dd822
danilospinola <danspinola10@gmail.com>
2025-01-22 17:01:12 -0300

    login uptdate

--- petitchef/src/components/Login.jsx
+++ petitchef/src/components/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import styles from "./Login.module.css";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, sendPasswordResetEmail,createUserWithEmailAndPassword  } from "firebase/auth";
 import { auth } from "../../firebase";
 
 function Login() {
@@ -80,9 +80,30 @@ function Login() {
             <p className={`${styles.forgotPass} ${styles.textDarker}`} onClick={redefinirSenha}>
               Esqueceu a senha?
             </p>
-            <button type="button" className={styles.submit}>
+            {!isSubmitted ? (
+            <button type="button" className={styles.submit} onClick={handleSignIn}>
               Entrar
             </button>
+            ) : (
+                <svg
+                  className={styles.checkmark}
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 52 52"
+                >
+                  <circle
+                    className={styles.checkmark__circle}
+                    cx="26"
+                    cy="26"
+                    r="25"
+                    fill="none"
+                  />
+                  <path
+                    className={styles.checkmark__check}
+                    fill="none"
+                    d="M14.1 27.2l7.1 7.2 16.7-16.8"
+                  />
+                </svg>
+              )}
           </div>
 
           <div className={styles.subCont}>
