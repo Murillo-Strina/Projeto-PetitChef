26773a1d1bab5fc2e957460725087ddd23b26a21
Murillo-Strina <143843776+Murillo-Strina@users.noreply.github.com>
2025-03-12 19:05:44 -0300

    Update vite.config.js

--- petitchef/vite.config.js
+++ petitchef/vite.config.js
@@ -4,14 +4,16 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/Projeto-PetitChef/',  
+  build: {
+    outDir: 'dist', 
+    emptyOutDir: true, 
+    chunkSizeWarningLimit: 1000,
+  },
   css: {
     modules: {
       scopeBehaviour: 'local',
       generateScopedName: '[name]__[local]___[hash:base64:5]',
       localsConvention: 'camelCase',
     },
-  },
-  build: {
-    chunkSizeWarningLimit: 1000,
   },
 });
