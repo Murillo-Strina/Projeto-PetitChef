1fbcf517aa8b8f9cc7bb326fb8e3687da28493d5
Murillo-Strina <143843776+Murillo-Strina@users.noreply.github.com>
2025-03-12 18:52:27 -0300

    Update App.jsx

--- petitchef/src/App.jsx
+++ petitchef/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Main from './components/Main';
-import Login from './components/login'; 
+import Login from './components/Login'; 
 
 function App() {
   return (
