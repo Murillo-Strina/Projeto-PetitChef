6fb94df6c8f9c920f7f3b2aa49c5790c5f819599
Murillo-Strina <143843776+Murillo-Strina@users.noreply.github.com>
2025-03-16 19:29:34 -0300

    Update vite.config.js

--- petitchef/vite.config.js
+++ petitchef/vite.config.js
@@ -1,13 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Projeto-PetitChef/',  
+  base: '/Projeto-PetitChef/',
   build: {
-    outDir: 'dist', 
-    emptyOutDir: true, 
+    outDir: 'dist',
+    emptyOutDir: true,
     chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+    },
   },
   css: {
     modules: {
